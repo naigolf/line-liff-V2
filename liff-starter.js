0af3b05b73611bb271873b58156034f8200ee00a
nai golf <niy_golf@hotmail.com>
2019-10-22 23:11:03 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false  //  true
-    const defaultLiffId = "1585135375-rgLlKOke";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1585135375-WJPm4jNX";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
